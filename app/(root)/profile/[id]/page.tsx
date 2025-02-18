355d9fb5214d374257f83d27e15dd27197e36b95
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-02-18 23:22:56 +0100

    add section hero to KidDetail page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Button from "@/components/Button";
 import kidsDB from "@/constants/kidsDB";
+import Image from "next/image";
 import React from "react";
 
 const Profile = async ({ params }: RouteParams) => {
@@ -13,10 +15,29 @@ const Profile = async ({ params }: RouteParams) => {
   const { pageTitle, pageDescription } = kidProfile;
 
   return (
-    <div className="h-screen bg-primary-lightBlue pt-40">
-      <h1 className="text-5xl">{pageTitle}</h1>
-      <p>{pageDescription}</p>
-    </div>
+    <>
+      <section>
+        <div className="container-large grid grid-cols-1 items-center justify-items-center gap-12 text-center lg:grid-cols-2 lg:justify-items-start lg:text-left">
+          <div className="mt-12 max-w-[38.5rem] lg:mt-0">
+            <h1 className="title-h1">{pageTitle}</h1>
+            <p className="mt-6 text-lg font-medium lg:text-2xl">
+              {pageDescription}
+            </p>
+            <div className="mt-8 flex justify-center gap-2 lg:justify-start">
+              <Button variant="primary" label="Donate Now" url="/" />
+              <Button variant="secondary" label="Learn more" url="/" />
+            </div>
+          </div>
+          <Image
+            src={kidProfile.coverImageSRC}
+            alt={kidProfile.name}
+            width={640}
+            height={640}
+            className="h-[40rem] w-[40rem] rounded-3xl border-4 border-primary-beige object-cover shadow-xxlarge"
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
