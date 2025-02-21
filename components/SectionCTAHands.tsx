aa8d3247697297f874f4d3d1d4bb69ec5fb5c61f
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-02-21 22:42:13 +0100

    add section CTA Hands

--- components/SectionCTAHands.tsx
+++ components/SectionCTAHands.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 const SectionCTAHands = () => {
   return (
-    <section className="section-padding">
+    <section className="section-padding max-lg:relative">
       <div className="container-large">
-        <div className="grid grid-cols-2">
-          <div>
+        <div className="items-center rounded-3xl lg:grid lg:grid-cols-2 lg:bg-white lg:shadow-xxlarge">
+          <div className="left-1/2 top-1/2 w-full max-w-[768px] p-6 max-lg:absolute max-lg:-translate-x-1/2 max-lg:-translate-y-1/2 max-lg:text-center max-sm:static max-sm:translate-x-0 max-sm:translate-y-0 lg:p-12">
             <SectionHeading
               title="Keep the Impact Going – Support Our Cause"
               subtitle="Your generosity helps provide essential medical aid, food, and support for children in need. Every contribution makes a difference."
@@ -18,7 +18,14 @@ const SectionCTAHands = () => {
               <Button variant="secondary" label="Get Involved" url="/404" />
             </div>
           </div>
-          <Image src={} alt="" width={656} height={437} layout="responsive" />
+          <Image
+            src="/images/SectionCTAHands/hands.svg"
+            alt="hands"
+            width={656}
+            height={437}
+            layout="responsive"
+            className="h-full w-full object-cover"
+          />
         </div>
       </div>
     </section>
