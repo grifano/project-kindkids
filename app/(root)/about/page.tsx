739fb74be6c59cff2f9c702a4f882604ec81d3a3
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-02-22 10:11:09 +0100

    add section text content with photo at About Page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,5 +1,6 @@
 import SectionCTAHands from "@/components/SectionCTAHands";
 import SectionVolonteers from "@/components/SectionVolonteers";
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
@@ -19,6 +20,62 @@ const About = () => {
               </p>
             </div>
           </div>
+        </div>
+      </section>
+      <section className="section-padding">
+        <div className="container-large">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
+            <h2 className="font-lora text-[2rem] font-medium leading-tight md:text-[3rem]">
+              From a Passion to a Purpose
+            </h2>
+            <div className="flex flex-col gap-4 max-lg:mt-6">
+              <p>
+                I was born and raised in Ukraine, where I had the privilege of
+                attending a Waldorf school—an environment that nurtured
+                creativity, empathy, and a deep appreciation for human
+                connection. It was there that I first encountered the struggles
+                faced by children with physical disabilities. Volunteering with
+                them as a student left a lasting imprint on me, shaping my
+                understanding of the challenges they face and inspiring a
+                lifelong desire to help.
+              </p>
+              <p>
+                Years later, after moving to Canada, that desire never faded. I
+                knew I had to take action—to reconnect with my roots and support
+                the children in Ukraine who continue to face immense hardships.
+                That’s how this initiative was born.
+              </p>
+              <p>
+                With a deep passion for{" "}
+                <strong>hospitality and the arts</strong>, I realized that a
+                <strong>charity dinner</strong> could be more than just an
+                event—it could be a movement.{" "}
+                <strong>
+                  event—it could be a movement. Food and art have always been my
+                  way of expressing care and connection event—it could be a
+                  movement.{" "}
+                </strong>
+                , and I wanted to channel these passions into something that
+                could truly make a difference.
+              </p>
+              <p>
+                This non-profit is my way of using the skills and experiences
+                I’ve gathered—creating unforgettable dining experiences,
+                <strong>
+                  curating artistic expression, and bringing people together—to
+                  help children in need.
+                </strong>
+              </p>
+            </div>
+          </div>
+          <Image
+            src="/images/section-about-content/maryna-with-timur-family.jpg"
+            width={1281}
+            height={720}
+            alt="Maryna tougether with Timur's family"
+            className="mt-10 rounded-3xl lg:mt-20"
+            layout="responsive"
+          />
         </div>
       </section>
       <SectionVolonteers />
