6c5f06a51d43f1561f72c87c55b2920248ba87dc
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-03-05 18:32:37 +0100

    add Layout01, complete page coming event

--- app/(root)/event/page.tsx
+++ app/(root)/event/page.tsx
@@ -1,4 +1,6 @@
+import Layout01 from "@/components/Layout01";
 import SectionEventHero from "@/components/SectionEventHero";
+import SectionHeading from "@/components/SectionHeading";
 import React from "react";
 
 const page = () => {
@@ -13,6 +15,40 @@ const page = () => {
         coverImage="/images/page-event/2024/bg-hero-2024.avif"
         coverAlt="woman draw a paint"
       />
+      <section className="section-padding">
+        <div className="container-large">
+          <SectionHeading
+            title="Join Us for an Evening of Hope and Generosity."
+            subtitle="Save the date for our 2025 charity dinner! Enjoy an unforgettable night of fine dining, inspiring art, and heartfelt giving as we come together to support children with disabilities in Ukraine. Your presence makes a difference."
+            className="m-auto max-w-[768px] text-center"
+          />
+          <div className="spacer-medium"></div>
+          {/* Layout Content -> Image */}
+          <Layout01
+            title="Charity Dinner with Purpose"
+            subTitle="Gather around the table for a thoughtfully prepared multi-course meal, made possible by our generous sponsors. This is a chance to connect, reflect, and discuss how we can make a difference."
+            imageUrl="/images/page-event/2025/temp-image.jpg"
+            imageAlt="temp image"
+          />
+          <div className="spacer-medium"></div>
+          {/* Layout Image -> Content */}
+          <Layout01
+            title="Live Art & Auctions"
+            subTitle="Witness the creation of a unique artwork, inspired by the cause. This painting, along with other donated items, will be auctioned to directly support children in need."
+            imageUrl="/images/page-event/2025/temp-image.jpg"
+            imageAlt="temp image"
+            isReverse
+          />
+          <div className="spacer-medium"></div>
+          {/* Layout Content -> Image */}
+          <Layout01
+            title="Raising Funds to Change Lives"
+            subTitle="Every ticket purchased, every auction bid, and every donation goes directly toward medical supplies, essential care, and support for families facing unimaginable hardships."
+            imageUrl="/images/page-event/2025/temp-image.jpg"
+            imageAlt="temp image"
+          />
+        </div>
+      </section>
     </>
   );
 };
---
+++ components/Layout01.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { title } from "process";
+import React, { FC } from "react";
+
+interface Layout01 {
+  title: string;
+  subTitle: string;
+  imageUrl: string;
+  imageAlt: string;
+  isReverse?: boolean;
+}
+
+const Layout01: FC<Layout01> = ({
+  title,
+  subTitle,
+  imageUrl,
+  imageAlt,
+  isReverse,
+}) => {
+  return isReverse ? (
+    <article className="flex flex-col-reverse items-center gap-4 text-center md:grid md:grid-cols-2 md:text-left lg:gap-12">
+      <div>
+        <h3 className="title-h3">{title}</h3>
+        <p className="text-medium mt-2 md:mt-6">{subTitle}</p>
+      </div>
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        width={616}
+        height={616}
+        layout="responsive"
+      />
+    </article>
+  ) : (
+    <article className="flex flex-col items-center gap-4 text-center md:grid md:grid-cols-2 md:text-left lg:gap-12">
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        width={616}
+        height={616}
+        layout="responsive"
+      />
+      <div>
+        <h3 className="title-h3">{title}</h3>
+        <p className="text-medium mt-2 md:mt-6">{subTitle}</p>
+      </div>
+    </article>
+  );
+};
+
+export default Layout01;
