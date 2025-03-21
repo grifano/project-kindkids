4f928dc15198cdb621735d649dae4ff7a0f4608e
Serhii Orlenko <146512960+grifano@users.noreply.github.com>
2025-03-21 18:27:03 +0100

    fix share buttons hover

--- components/ui/ShareButtons.tsx
+++ components/ui/ShareButtons.tsx
@@ -16,48 +16,48 @@ interface ShareButtonsProps {
 }
 
 const ShareButtons: FC<ShareButtonsProps> = ({ url }) => {
-  const iconBgStyle = { fill: "#F9F6FC" };
+  const iconBgStyle = { fill: "currentColor" };
 
   return (
     <ul className="flex items-center justify-center gap-2">
-      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full transition-colors hover:bg-primary-purple">
+      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full transition-colors">
         <button
-          className="hover: flex h-[32px] w-[32px] items-center justify-center rounded-full bg-primary-lightPurple"
+          className="flex h-[32px] w-[32px] items-center justify-center rounded-full bg-primary-deepBlue text-white hover:bg-primary-purple"
           onClick={() => {
             navigator.clipboard.writeText(url);
             alert("Link copied to clipboard!");
           }}
         >
-          <FaLink className="text-primary-purple" />
+          <FaLink className="text-inherit" />
         </button>
       </li>
-      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full transition-colors hover:bg-primary-purple">
+      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full text-primary-deepBlue transition-colors hover:text-primary-purple">
         <FacebookShareButton url={url}>
           <FacebookIcon
             bgStyle={iconBgStyle}
             size={32}
             round
-            iconFillColor="#A88BCB"
+            // iconFillColor="#A88BCB"
           />
         </FacebookShareButton>
       </li>
-      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full transition-colors hover:bg-primary-purple">
+      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full text-primary-deepBlue transition-colors hover:text-primary-purple">
         <LinkedinShareButton url={url}>
           <LinkedinIcon
             bgStyle={iconBgStyle}
             size={32}
             round
-            iconFillColor="#A88BCB"
+            // iconFillColor="#A88BCB"
           />
         </LinkedinShareButton>
       </li>
-      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full transition-colors hover:bg-primary-purple">
+      <li className="flex h-[36px] w-[36px] items-center justify-center rounded-full text-primary-deepBlue transition-colors hover:text-primary-purple">
         <TwitterShareButton url={url}>
           <TwitterIcon
             bgStyle={iconBgStyle}
             size={32}
             round
-            iconFillColor="#A88BCB"
+            // iconFillColor="#A88BCB"
           />
         </TwitterShareButton>
       </li>
